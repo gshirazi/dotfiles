61f2bb0dd2b7f63b980870635d09d80c1a258c30
Ghasem Shirazi <gshirazi@infoblox.com>
2026-02-26 12:25:37 -0800

    Add git aliases for pull commands
    Added new git aliases for pulling from master and current branch.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
+alias git='git --no-pager'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -22,3 +23,6 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+alias glom='git pull origin master' 
+alias glomain='git pull origin $(git_main_branch)'
+alais gloc='git pull origin $(git_current_branch)
